f48571a7b4a02f88c757a4ccf54fdea8b9b24304
Pradip Mudi <pradipmudi24@gmail.com>
2021-01-30 21:27:41 +0530

    Spring Boot and WebSocket based Chat Application with JUnit4

---
+++ src/main/java/com/vocera/ws/api/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.vocera.ws.api.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.vocera.ws.api.model.Message;
+
+@Controller
+public class ChatController {
+	
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public Message register(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", message.getSender());
+		return message;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public Message sendMessage(@Payload Message message) {
+		return message;
+	}
+
+}
---
+++ src/test/java/com/vocera/ws/api/MessagingClientApplicationTests.java
@@ -0,0 +1,69 @@
+package com.vocera.ws.api;
+
+import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+/**
+ * Created by rhzeffa on 16/05/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MessagingClientApplicationTests {
+
+    static final String WEBSOCKET_URI = "ws://localhost:8080/vocera";
+    static final String WEBSOCKET_TOPIC = "/topic/public";
+
+    BlockingQueue<String> blockingQueue;
+    WebSocketStompClient stompClient;
+
+    @Before
+    public void setup() {
+        blockingQueue = new LinkedBlockingDeque<>();
+        stompClient = new WebSocketStompClient(new SockJsClient(
+                asList(new WebSocketTransport(new StandardWebSocketClient()))));
+    }
+
+    @Test
+    public void shouldReceiveAMessageFromTheServer() throws Exception {
+        StompSession session = stompClient
+                .connect(WEBSOCKET_URI, new StompSessionHandlerAdapter() {})
+                .get(1, SECONDS);
+        session.subscribe(WEBSOCKET_TOPIC, new DefaultStompFrameHandler());
+
+        String message = "MESSAGE TEST";
+        session.send(WEBSOCKET_TOPIC, message.getBytes());
+
+        Assert.assertEquals(message, blockingQueue.poll(1, SECONDS));
+    }
+
+    class DefaultStompFrameHandler implements StompFrameHandler {
+        @Override
+        public Type getPayloadType(StompHeaders stompHeaders) {
+            return byte[].class;
+        }
+
+        @Override
+        public void handleFrame(StompHeaders stompHeaders, Object o) {
+            blockingQueue.offer(new String((byte[]) o));
+        }
+    }
+}
